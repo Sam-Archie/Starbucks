1c10b9c7a18344c1de5e7aea1c4b25fea4c1c361
Sam Archie <samuelarchie1@gmail.com>
2021-02-04 10:34:40 +0000

    gulpfile settings for minification and SASS

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass');
+
+
+gulp.task('minify-css', () => {
+    return gulp.src('css/styles.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./css/'));
+  });
+
+gulp.task('sass', function () {
+	return gulp.src('./scss/styles.scss')
+    .pipe(sass())
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task("minify-sass", gulp.series("sass", "minify-css"));
+
+gulp.task("watch", () => {
+    return gulp.watch("scss/**/*.scss", 
+            gulp.task("minify-sass"));
+});
